8a220d4cf8a2df17bb1302ac002053f79129da4c
Marcos Andre <marcosdev.me@gmail.com>
2022-02-09 08:40:06 -0300

    chore: Add Styled-Components

---
+++ pages/dashboard/index.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: tomato;
+`;
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Title>Dashboard Area</Title>
+    </div>
+  );
+}
