fa6209a5986ff392efc6e6b5a4bae3c594951198
Marcos Andre <marcosdev.me@gmail.com>
2022-02-09 09:21:10 -0300

    feat: add Colors

--- components/Layout/styles.jsx
+++ components/Layout/styles.jsx
@@ -2,16 +2,23 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  background: #414141;
+  min-height: 100vh;
+  color: #fff;
 `;
 
 export const LeftSide = styled.div`
   width: 18%;
-  background: tomato;
   min-height: 100vh;
+  border: 1px solid #898989;
+  border-left: none;
+  border-bottom: none;
 `;
 
 export const RightSide = styled.div`
   flex: 1;
-  background: darkblue;
   min-height: 100vh;
+  border: 1px solid #898989;
+  border-left: none;
+  border-bottom: none;
 `;
