357693f172a2a52e8c75e0bc27dd1154fbfb0a70
Marcos Andre <marcosdev.me@gmail.com>
2022-02-14 15:39:14 -0300

    feat: creating empty array for initial data

--- data/product.jsx
+++ data/product.jsx
@@ -1,13 +1,3 @@
-import { v4 as uuid } from "uuid";
-
-const data = [
-  {
-    id: uuid(),
-    productName: "first test",
-    category: "Compra",
-    totalValue: 249,
-    dateMade: "02-2021",
-  },
-];
+const data = [];
 
 export default data;
