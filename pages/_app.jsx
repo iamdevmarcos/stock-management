0a43e67d4cb71ee3195556f4b8c797afc30b022e
Marcos Andre <marcosdev.me@gmail.com>
2022-02-27 16:26:57 -0300

    feat: add Global Styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,14 @@
-import "../styles/globals.css";
 import { Provider } from "react-redux";
 import { ChakraProvider } from "@chakra-ui/react";
+import { GlobalStyles } from "../styles/GlobalStyles";
 
 import store from "../store/configureStore";
 
 function myApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <GlobalStyles />
+
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
---
+++ styles/GlobalStyles.jsx
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: "Poppins", sans-serif !important;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .sidebar-items:not(:last-child) {
+    margin-bottom: 1.5em;
+  }
+
+  @media only screen and (max-width: 767px) {
+    .sidebar-items {
+      margin-bottom: 0 !important;
+    }
+  }
+
+  .sidebar-items {
+    color: gray;
+    font-size: large;
+  }
+
+  .sidebar-items p {
+    margin-left: 1em;
+  }
+
+  .sidebar-items:hover svg {
+    color: #b57295;
+  }
+
+  .sidebar-items:hover p {
+    color: #fff;
+  }
+
+  .active {
+    color: #fff;
+  }
+
+  .active-icon {
+    color: #b57295 !important;
+  }
+`;
